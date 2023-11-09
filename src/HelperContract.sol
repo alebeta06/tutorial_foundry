c38336ea6144164d054d70e393dc2c9bf53e8107
alebeta <alebeta0000@gmail.com>
2023-11-09 15:07:08 -0500

    rama con mas test

---
+++ src/HelperContract.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+abstract contract HelperContract {
+    address constant IMPORTANT_ADDRESS = 0x377307274977ac9E3b2b71BEaA8a1aC859FB1097;
+}
