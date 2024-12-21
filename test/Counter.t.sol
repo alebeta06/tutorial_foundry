cb127c5dbc09cc09d147b6f3522882e9e953c601
alebeta <alebeta0000@gmail.com>
2024-12-20 23:42:11 -0500

    modificando Counter

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,26 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test, console } from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
-contract CounterTest is Test, HelperContract {
+contract CounterTest is Test {
     Counter public counter;
-    uint256 testNumber;
-    address importantAddress;
 
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
+    
     }
 
     function test_Increment() public {
         counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        console.log("imprime el siguiente numero :", 256);
+        assertEq(counter.contador(), 1);
     }
 }
