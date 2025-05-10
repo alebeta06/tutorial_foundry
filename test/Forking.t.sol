b039a8da598ffe4303f03c2b5f4306aa26fefb74
alebeta06 <alebeta0000@gmail.com>
2025-05-10 14:05:41 -0500

    Add test for minting DAI and skip deposit test execution

--- test/Forking.t.sol
+++ test/Forking.t.sol
@@ -24,7 +24,8 @@ contract ForkingTest is Test {
         DAI = IDAI(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     }
 
-    function testDepositEtherMintWETH() public {        
+    function testDepositEtherMintWETH() public {
+        vm.skip(true);        
         vm.deal(address(this), 1 ether);
         uint256 balance = WETH.balanceOf(address(this));
         emit log_uint(balance);
@@ -33,5 +34,22 @@ contract ForkingTest is Test {
         uint256 finalBalance = WETH.balanceOf(address(this));
         emit log_uint(finalBalance / 1e18);
         assertEq(finalBalance, 1 ether);
+    }
+
+    function testMintDai() public {
+        address alebeta = makeAddr("alebeta");
+        uint256 balance = DAI.balanceOf(alebeta);
+        uint256 initialTotalSupply = DAI.totalSupply();
+
+        emit log_uint(balance);
+        emit log_uint(initialTotalSupply / 1e18);
+
+        deal(address(DAI), alebeta, 1 ether, true);
+
+        uint256 finalBalance = DAI.balanceOf(alebeta);
+        uint256 finalTotalSupply = DAI.totalSupply();
+
+        emit log_uint(finalBalance / 1e18);
+        emit log_uint(finalTotalSupply / 1e18);
     }
 }
