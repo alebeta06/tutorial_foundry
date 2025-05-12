156682ecdd776642ef8970f3d0a91bf287ff4192
alebeta06 <alebeta0000@gmail.com>
2025-05-11 19:37:42 -0500

    Initialize total variable in Counter2 contract and add usage instructions for local node setup and contract deployment.

--- src/Counter2.sol
+++ src/Counter2.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 contract Counter2 {
     uint256 public number;
-    uint256 public total;
+    uint256 public total = 80;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
@@ -16,3 +16,16 @@ contract Counter2 {
     
     
 }
+
+//en otra terminal
+//~/tutorial_foundry$ anvil
+//para iniciar el nodo local y octener la private key
+
+//:~/tutorial_foundry$ forge create Counter2 --private-key <private-key> --broadcast
+
+//Transaction: 0x4d00d46ce455b184c970ce28cb0c917f0f761541636f8c6089e2c7e3db8305ef
+//Contract created: 0x700b6A60ce7EaaEA56F065753d8dcB9653dbAD35
+//Gas used: 205389
+
+//:~/tutorial_foundry$ cast call 0x700b6A60ce7EaaEA56F065753d8dcB9653dbAD35 "total()"
+//0x0000000000000000000000000000000000000000000000000000000000000050 (es el total 80)
