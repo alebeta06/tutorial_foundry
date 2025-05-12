92af708e0ad708bb26215fabca2955988bd57cc5
alebeta06 <alebeta0000@gmail.com>
2025-05-12 15:07:47 -0500

    [ADD ExampleContract1.sol] Invariant Testing - Open Testing

---
+++ src/ExampleContract1.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+contract ExampleContract1 {
+    uint256 public value1;
+    uint256 public value2;
+    uint256 public value3;
+
+    function addToA(uint256 _value) public {
+        value1 += _value;
+        value3 += _value;
+    }
+
+    function addToB(uint256 _value) public {
+        value2 += _value;
+        value3 += _value;
+    }
+}
